966f30a3057fd3409280dbd76dde57746e29dc42
meryembenassila <benassila.meryem08@gmail.com>
2026-05-11 12:12:49 +0100

    securisé les methodes

--- backend/BenassilaMeryem/src/main/java/benassila/meryem/benassilameryem/web/ClientRestController.java
+++ backend/BenassilaMeryem/src/main/java/benassila/meryem/benassilameryem/web/ClientRestController.java
@@ -4,6 +4,7 @@ import benassila.meryem.benassilameryem.dtos.ClientDto;
 import benassila.meryem.benassilameryem.exceptions.ClientNotFoundException;
 import benassila.meryem.benassilameryem.service.ClientService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,25 +17,26 @@ public class ClientRestController {
 
 
 
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     @GetMapping("/clients")
     List<ClientDto> getClients(){
         return clientService.getClients();
     }
 
-
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     @GetMapping("/clients/search")
     List<ClientDto> search(@RequestParam(name = "motif", defaultValue = "")String motif){
         return clientService.search(motif);
     }
 
-
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     @DeleteMapping("/clients/delete/{id}")
     void deleteClient(@PathVariable Long id) throws ClientNotFoundException {
         clientService.deleteClient(id);
     }
 
 
-
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     @PostMapping("/clients/save")
     ClientDto saveClient(@RequestBody ClientDto clientDto){
         return clientService.saveClient(clientDto);
